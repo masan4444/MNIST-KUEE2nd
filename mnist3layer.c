132b3d9d463ffef67f3654a5f2b9b4a9d4298a5a
masan4444 <masanori0213@gmail.com>
2017-07-15 17:35:25 +0900

    関数コメントの追加

--- mnist3layer.c
+++ mnist3layer.c
@@ -28,11 +28,13 @@ int main(int argc, char const * argv[]) {
 
 /**
  * @fn
- * 確率的勾配法を用いて学習する
- * @param (int epoch) エッポク数
- * @param (int batch_size) rand()のシード値
- * @param (float * o) 初期化する配列
+ * 確率的勾配法を用いて学習する．ただしエッポクが進むごとに学習率を変化させている．
+ * @param (int epoch) エッポクの総数
+ * @param (int batch_size) batchのサイズ
+ * @param (float initial_learning_rate) 最初の学習率
+ * @param (const char * filename) 学習したパラメータを保存するファイル名
  * @return 無し
+ * @detail エッポクごとに学習率を減少させていくことで効率的に学習している．具体的にはエッポク数nの時の学習率を「最初の学習率 / n」としている
  */
 void SGD(int epoch, int batch_size, float initial_learning_rate, const char * filename) {
     float * train_x = NULL;
@@ -103,6 +105,17 @@ void SGD(int epoch, int batch_size, float initial_learning_rate, const char * fi
    }
 }
 
+/**
+ * @fn
+ * 確率的勾配降下法を用いて学習する．ただし更新に慣性項と呼ばれるものを付与している．
+ * @param (int epoch) エッポクの総数
+ * @param (int batch_size) batchのサイズ
+ * @param (float learning_rate) 学習率
+ * @param (float momentum) どれだけ慣性を付与するかのハイパーパラメータ
+ * @param (const char * filename) 学習したパラメータを保存するファイル名
+ * @return 無し
+ * @detail パラメータ更新の際に，前回のパラメータの更新量にmomentumをかけた量を追加することで，学習をより慣性的なものにしている．
+ */
 void MomentumSGD(int epoch, int batch_size, float learning_rate, float momentum, const char * filename) {
     float * train_x = NULL;
     unsigned char * train_y = NULL;
@@ -191,10 +204,22 @@ void MomentumSGD(int epoch, int batch_size, float learning_rate, float momentum,
     }
 }
 
+/**
+ * @fn
+ * fc -> relu -> softmax の3層で推論する
+ * @param (const float * A) 重みパラメータ，行列Aの配列
+ * @param (const float * b) バイアスパラメータ，ベクトルbの配列
+ * @param (const float * x) 入力ベクトルの配列
+ * @param (float * y) 出力ベクトルの配列
+ * @return ネットワークによって推論される数字(0 ~ 9)
+ * @detail 引数に出力ベクトルの配列を追加することで，下記のaccRate_and_loss関数のおいて，正解率と損失関数を同時に求めることが出来た．
+ */
+
 int inference3(const float * A, const float * b, const float * x, float * y) {
     init(10, 0, y);
     fc(10, 784, x, A, b, y);
     relu(10, y, y);
+    softmax(10, y, y);
     float max = y[0];
     int ans = 0;
     int i;
@@ -207,6 +232,18 @@ int inference3(const float * A, const float * b, const float * x, float * y) {
     return ans;
 }
 
+/**
+ * @fn
+ * fc -> relu -> softmax の3層のニューラルネットワークを，誤差逆伝播法を用いて偏微分を求める
+ * @param (const float * A) 重みパラメータ，行列Aの配列
+ * @param (const float * b) バイアスパラメータ，ベクトルbの配列
+ * @param (const float * x) 入力ベクトルの配列
+ * @param (unsigned char t) 正解ラベル(0 ~ 9)
+ * @param (float * y) 出力ベクトルの配列
+ * @param (float * dA) 重みパラメータAの偏微分の配列
+ * @param (float * db) バイアスパラメータbの偏微分の配列
+ * @return 無し
+ */
 void backward3(const float * A, const float * b, const float * x, unsigned char t, float * y, float * dA, float * db) {
     fc(10, 784, x, A, b, y);
     float * x_relu = malloc(FLOAT_SIZE*10);
@@ -226,6 +263,18 @@ void backward3(const float * A, const float * b, const float * x, unsigned char 
     free(dx_fc);
 }
 
+/**
+ * @fn
+ * fc -> relu -> softmax の3層のニューラルネットワークに対して，テストデータの正解率と損失関数を表示する
+ * @param (const float * A) 重みパラメータ，行列Aの配列
+ * @param (const float * b) バイアスパラメータ，ベクトルbの配列
+ * @param (const float * test_x) 入力ベクトル(テストデータ)をすべて集めた配列
+ * @param (const unsigned char * test_y) 正解ラベル(テストデータ)をすべて集めた配列
+ * @param (float * accRate) 正解率を格納するポインタ
+ * @param (float * Loss) 損失関数の値を格納するポインタ
+ * @return 無し
+ * @detail inference関数で出力ベクトルを求めることで，正解率と損失関数を同時に求めるようにしている．そのため正解率と損失関数の値はポインタで取得している．
+ */
 void accRate_and_loss(const float * A, const float * b, const float * test_x, const unsigned char * test_y, float * accRate, float * Loss) {
     int sum_acc = 0;
     float sum_loss = 0;
@@ -241,6 +290,14 @@ void accRate_and_loss(const float * A, const float * b, const float * test_x, co
     *Loss = sum_loss / 10000;
 }
 
+/**
+ * @fn
+ * fc -> relu -> softmax の3層のニューラルネットワークで，与えられた画像に書かれている数字を推論し，表示する．
+ * @param (const char * filename) パラメータが保存されているファイル名
+ * @param (const char * bmp_filename) 数字が書かれている画像ファイル名
+ * @param (const float * x) 入力ベクトルの配列
+ * @return 無し
+ */
 void inferenceMode(const char * filename, const char * bmp_filename) {
     float * A = malloc(FLOAT_SIZE*10*784);
     float * b = malloc(FLOAT_SIZE*10);
